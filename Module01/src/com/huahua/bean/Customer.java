601b25b60e3fb4cbb8a95c9df432d77a940f4b6b
LWF <517397344@qq.com>
2019-07-06 12:28:59 +0800

    Initial commit

---
+++ Module01/src/com/huahua/bean/Customer.java
@@ -0,0 +1,10 @@
+package com.huahua.bean;
+
+
+//123456
+public class Customer {
+    public  static  void main(String[] args){
+        System.out.println("hellowWorld ");
+
+    }
+}
---
+++ src/Hello.java
@@ -0,0 +1,6 @@
+/**
+ * @author lwf
+ * @date 2019/7/5 17:28
+ */
+public class Hello {
+}
